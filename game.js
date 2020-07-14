b135153ad8913f2dcdd2b6335626d0d4aa07e170
nhcarrigan <nhcarrigan@gmail.com>
2020-07-14 15:03:48 -0700

    Game is done!

---
+++ game.js
@@ -0,0 +1,34 @@
+let play
+function start() {
+  const target = document.getElementById("target").value;
+  const score = document.getElementById("score");
+  score.innerText = target;
+  play = setInterval(generateBug, 2000);
+}
+
+function generateBug() {
+  const random = Math.floor(Math.random() * 8) + 1;
+  console.log(random);
+  const box = document.getElementById(`${random}`);
+  if (box.innerHTML) generateBug();
+  box.innerHTML = `<img class="bug" src="./img/bug.png" onclick="smash(${random})">`;
+}
+
+function smash(num) {
+  const score = document.getElementById("score");
+  score.innerText = parseInt(score.innerText) - 1;
+  if (parseInt(score.innerText) == 0) end()
+  const box = document.getElementById(`${num}`);
+  box.innerHTML = `<img class="bug" src="./img/squish.png">`;
+  setTimeout(clear, 2000, num);
+}
+
+function clear(num) {
+  const box = document.getElementById(`${num}`);
+  box.innerHTML = "";
+}
+
+function end() {
+    clearInterval(play)
+    alert("You squashed all the bugs! Now go squash the real bugs in your code ;)")
+}
