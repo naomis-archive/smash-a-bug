896b9092309e3f025e11dfbe734d3693334a758c
nhcarrigan <nhcarrigan@gmail.com>
2020-07-14 15:58:17 -0700

    Fix persistent bugs - closes #2

--- game.js
+++ game.js
@@ -1,5 +1,15 @@
 let play;
 function start() {
+  clearInterval(play)
+  document.getElementById(1).innerHTML = "";
+  document.getElementById(2).innerHTML = "";
+  document.getElementById(3).innerHTML = "";
+  document.getElementById(4).innerHTML = "";
+  document.getElementById(5).innerHTML = "";
+  document.getElementById(6).innerHTML = "";
+  document.getElementById(7).innerHTML = "";
+  document.getElementById(8).innerHTML = "";
+  document.getElementById(9).innerHTML = "";
   const target = document.getElementById("target").value;
   if (!(parseInt(target) > 0))
     return alert("Please enter a target number greater than 0.");
